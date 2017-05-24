b4b4fe5566a3a134c9a64103eede15503d227bba
Dhaval <dhaval.maru@otbconsulting.co.in>
2017-05-24 10:13:17 +0530

    db changes

---
+++ database/OTB-primarc-accounting-9.sql
@@ -0,0 +1,10 @@
+truncate table grn_acc_entries;
+truncate table grn_acc_sku_entries;
+truncate table grn_debit_notes;
+truncate table journal_voucher_details;
+truncate table journal_voucher_entries;
+truncate table ledger_entries;
+truncate table payment_receipt_details;
+truncate table payment_advices;
+update series_master set series = 0 where type = 'Voucher';
+alter table ledger_entries add column ref_date date null;
