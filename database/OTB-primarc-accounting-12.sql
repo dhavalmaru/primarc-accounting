3878b3e10678bb3a1ce2203acef627a66b8575b3
Dhaval <dhaval.maru@otbconsulting.co.in>
2017-06-01 10:27:45 +0530

    db insert role

---
+++ database/OTB-primarc-accounting-12.sql
@@ -0,0 +1,2 @@
+INSERT INTO `acc_user_role_master` (`id`, `role`, `description`, `status`, `is_active`, `created_by`, `created_date`, `updated_by`, `updated_date`) VALUES
+(1, 'Admin', 'Admin', 'approved', 1, 46, '2017-05-23 00:00:00', 127, '2017-05-23 07:49:58');
