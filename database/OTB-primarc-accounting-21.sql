ba1526f768f2652ff930b44377110398058d64e4
Dhaval <dhaval.maru@otbconsulting.co.in>
2017-08-24 19:53:37 +0530

    email log

---
+++ database/OTB-primarc-accounting-21.sql
@@ -0,0 +1,4 @@
+INSERT INTO `acc_user_role_options` (`id`, `role_id`, `r_section`, `r_view`, `r_insert`, `r_edit`, `r_delete`, `r_approval`, `r_export`) VALUES (NULL, '1', 'S_Email_Log', '1', '1', '1', '1', '1', '1');
+INSERT INTO `acc_user_role_options` (`id`, `role_id`, `r_section`, `r_view`, `r_insert`, `r_edit`, `r_delete`, `r_approval`, `r_export`) VALUES (NULL, '2', 'S_Email_Log', '1', '0', '0', '0', '0', '0');
+INSERT INTO `acc_user_role_options` (`id`, `role_id`, `r_section`, `r_view`, `r_insert`, `r_edit`, `r_delete`, `r_approval`, `r_export`) VALUES (NULL, '3', 'S_Email_Log', '1', '1', '1', '1', '0', '0');
+INSERT INTO `acc_user_role_options` (`id`, `role_id`, `r_section`, `r_view`, `r_insert`, `r_edit`, `r_delete`, `r_approval`, `r_export`) VALUES (NULL, '4', 'S_Email_Log', '1', '1', '1', '1', '1', '0');
