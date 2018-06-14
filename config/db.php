6e95f7d871801397727b02637c85d9a5a0f795c0
prasadb0910 <prasadb0910@gmail.com>
2018-06-14 13:17:47 +0530

    Database Path Changes

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=primarc',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'readuser',
+    'password' => 'xqxR5sYwvF1y8AYaqmQM',
     'charset' => 'utf8',
 ];
